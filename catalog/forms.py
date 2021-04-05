c5e0cc9ab3034b5eed9d33d11c33b0f92ef7c5e6
benjaminhonorio <benjaminhonorio.12@gmail.com>
2021-04-05 00:18:51 -0500

    Updated forms for renewals and change status (mainly), due_back and borrower of book instances

--- catalog/forms.py
+++ catalog/forms.py
@@ -6,52 +6,52 @@ from django.utils.translation import ugettext_lazy as _
 from django.forms import ModelForm
 from catalog.models import BookInstance
 
-class RenewBookForm(forms.Form):
-    renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
-    def clean_renewal_date(self):
-        data = self.cleaned_data['renewal_date']
-
-        # Check if a date is not in the past.
-        if data < datetime.date.today():
-            raise ValidationError(_('Invalid date - renewal in past'))
-
-        # Check if a date is in the allowed range (+4 weeks from today).
-        if data > datetime.date.today() + datetime.timedelta(weeks=4):
-            raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
-
-        # Remember to always return the cleaned data.
-        return data
-
 class RenewBookModelForm(ModelForm):
     class Meta:
         model = BookInstance
-        fields = ['borrower', 'due_back', 'status']
-        labels = {
-            'due_back': _('New renewal date'), 
-            'status': _('Status'), 
-            'borrower': _('Borrower')}
-        help_texts = {
-            'due_back': _('Enter a date between now and 4 weeks (default 3).'),
-            'status':_('Change status if needed.'), 
-            'borrower':_('Change borrower if needed.')} 
+        fields = ['due_back']
+        labels = {'due_back': _('New renewal date'),}
+        help_texts = {'due_back': _('Enter a date between now and 4 weeks (default 3).'),} 
     
     def clean_due_back(self):
         data = self.cleaned_data['due_back']
-
-        # Check if a date is not in the past.
+        # Check if a date is not in the past and is not None.
         if data is not None and data < datetime.date.today():
             raise ValidationError(_('Invalid date - renewal in past'))
-
-        # Check if a date is in the allowed range (+4 weeks from today).
+        # Check if a date is in the allowed range (+4 weeks from today) and is not None.
         if data is not None and data > datetime.date.today() + datetime.timedelta(weeks=4):
             raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
-
         # Remember to always return the cleaned data.
         return data
 
 class StatusBookModelForm(ModelForm):
     class Meta:
         model = BookInstance
-        fields = ['status']
-        labels = {'status': _('Change status')}
-        help_texts = {'status': _('Select the status for this book')} 
+        fields = ['due_back', 'status','borrower']
+        labels = {
+            'due_back': _('Return date'),
+            'status': _('Status'), 
+            'borrower': _('Borrower'),
+            }
+        help_texts = {
+            'due_back': _('If needed, enter a date between now and 4 weeks.'),
+            'status': _('Select the status for this book'),  
+            'borrower':_('Add borrower if needed.'),
+            } 
+
+# Alternative form for RenewBookModelForm
+# class RenewBookForm(forms.Form):
+#     renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
+#     def clean_renewal_date(self):
+#         data = self.cleaned_data['renewal_date']
+
+#         # Check if a date is not in the past.
+#         if data < datetime.date.today():
+#             raise ValidationError(_('Invalid date - renewal in past'))
+
+#         # Check if a date is in the allowed range (+4 weeks from today).
+#         if data > datetime.date.today() + datetime.timedelta(weeks=4):
+#             raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
+
+#         # Remember to always return the cleaned data.
+#         return data
